f400b0d46f3273d1bff17fca925d76212ff19fef
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-16 14:58:44 +0300

    Add FeedbackForm component with character limit functionality

--- src/components/FeedbackForm.tsx
+++ src/components/FeedbackForm.tsx
@@ -1,17 +1,39 @@
+import { useState } from 'react';
+import { MAX_CHARACTERS } from './lib/constants';
+
 export default function FeedbackForm() {
+  const [inputValue, setInputValue] = useState('');
+
+  const charCount = MAX_CHARACTERS - inputValue.length;
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log('Feedback submitted:', inputValue);
+    setInputValue('');
+  };
+
+  const handleOnChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    const newValue = e.currentTarget.value;
+    if (newValue.length > MAX_CHARACTERS) {
+      return;
+    }
+    setInputValue(newValue);
+  };
   return (
-    <form className='form'>
+    <form onSubmit={handleSubmit} className='form'>
       <textarea
         id='feedback-textarea'
         placeholder='blabla'
         spellCheck={false}
+        value={inputValue}
+        onChange={handleOnChange}
       />
       <label htmlFor='feedback-textarea'>
         Enter your feedback here, remember to #hashtag the company
       </label>
 
       <div>
-        <p className='u-italic'>150</p>
+        <p className='u-italic'>{charCount}</p>
         <button>
           <span>Submit</span>
         </button>
