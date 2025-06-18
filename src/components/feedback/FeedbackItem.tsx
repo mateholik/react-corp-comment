92a81ba13b9a81d3312b1d3dc9a5ae214a391652
Vladimir Vaisov <vaisov.vladimir@gmail.com>
2025-06-18 10:33:44 +0300

    Refactor FeedbackItem component: add upvote functionality and expand/collapse feature

--- src/components/feedback/FeedbackItem.tsx
+++ src/components/feedback/FeedbackItem.tsx
@@ -1,16 +1,31 @@
 import { TriangleUpIcon } from '@radix-ui/react-icons';
 import type { TFeedbackItem } from '../lib/types';
+import { useState } from 'react';
 
 type FeedbackItemProps = {
   feedbackItem: TFeedbackItem;
 };
 
 export default function FeedbackItem({ feedbackItem }: FeedbackItemProps) {
+  const [open, setOpen] = useState(false);
+  const [upvoteCount, setUpvoteCount] = useState(feedbackItem.upvoteCount);
+
+  const handleClickUpvote = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setUpvoteCount((prev) => prev + 1);
+    event.stopPropagation();
+    event.currentTarget.disabled = true;
+  };
+
   return (
-    <li className='feedback'>
-      <button>
+    <li
+      className={`feedback ${open ? 'feedback--expand' : ''}`}
+      onClick={() => setOpen((prev) => !prev)}
+    >
+      <button onClick={handleClickUpvote}>
         <TriangleUpIcon />
-        <span>{feedbackItem.upvoteCount}</span>
+        <span>{upvoteCount}</span>
       </button>
       <div>
         <p>{feedbackItem.badgeLetter}</p>
